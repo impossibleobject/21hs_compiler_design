1589299ad7691f381500c48bfae5e9e8642fd48b
leon <plathl@student.ethz.ch>
2021-10-15 17:11:04 +0200

    removed duplicate and wrong functions

--- hw2/simulator.ml
+++ hw2/simulator.ml
@@ -219,7 +219,7 @@ let quad_into_reg (q:quad) (regs:regs) (r:reg) : unit =
   Array.set regs (rind r) q
 
 (*F: pattern matching for movq*)
-let movq_helper (op1:operand) (op2:operand) (r:regs) (m:mem) : unit =
+let set_mem (op1:operand) (op2:operand) (r:regs) (m:mem) : unit =
   begin match op2 with
     | Reg reg -> quad_into_reg (interp_op op1 r m) r reg
     | (Imm x | Ind1 x) -> quad_sbyte_list_into_mem (sbytes_of_int64 (interp_op op1 r m)) m (get_addr (Imm x) r)
@@ -231,13 +231,13 @@ let movq_helper (op1:operand) (op2:operand) (r:regs) (m:mem) : unit =
 let pushq_helper (op:operand) (r:regs) (m:mem) : unit =
   let rsp_new_val = Int64.sub r.(rind Rsp) 8L in
   r.(rind Rsp) <- rsp_new_val;
-  movq_helper op (quad_to_imm (r.(rind Rsp))) r m
+  set_mem op (quad_to_imm (r.(rind Rsp))) r m
 
 (*F: helper function for popq*)
 let popq_helper (op:operand) (r:regs) (m:mem) : unit =
   let rsp_old_val = r.(rind Rsp) in
   let rsp_new_val = Int64.add rsp_old_val 8L in
-  movq_helper (quad_to_imm (r.(rind Rsp))) op r m;
+  set_mem (quad_to_imm (r.(rind Rsp))) op r m;
   r.(rind Rsp) <- rsp_new_val
 
 (*F: helper function for leaq*)
@@ -246,17 +246,17 @@ let leaq_helper (ind:operand) (op2:operand) (r:regs) (m:mem) : unit =
     | Imm i -> failwith "leaq called with non-indirect operand type!"
     | Reg i -> failwith "leaq called with non-indirect operand type!"
     | _ -> let effective_addr = get_addr ind r in
-        movq_helper (quad_to_imm (Int64.of_int effective_addr)) op2 r m
+        set_mem (quad_to_imm (Int64.of_int effective_addr)) op2 r m
   end
 
 (*F: helper function for jmp*)
 let jmp_helper (op:operand) (r:regs) (m:mem) : unit = 
-  movq_helper op (Reg Rip) r m
+  set_mem op (Reg Rip) r m
 
 (*F: helper function for callq*)
 let callq_helper (op:operand) (r:regs) (m:mem) : unit =
   pushq_helper (Reg Rip) r m;
-  movq_helper op (Reg Rip) r m 
+  set_mem op (Reg Rip) r m 
 
 (*F: helper function for retq*)
 let retq_helper (r:regs) (m:mem) : unit =
@@ -266,8 +266,8 @@ let retq_helper (r:regs) (m:mem) : unit =
 let jcc_helper (cc:cnd) (op:operand) (r:regs) (m:mem) (f:flags) : unit =
   let fallthru_rip = quad_to_imm (Int64.add r.(rind Rip) 8L) in
   if interp_cnd f cc 
-    then movq_helper op (Reg Rip) r m 
-    else movq_helper fallthru_rip (Reg Rip) r m 
+    then set_mem op (Reg Rip) r m 
+    else set_mem fallthru_rip (Reg Rip) r m 
 
 (*F: helper function for setb 
   -> assumes 'lower byte of dest' means the byte with the smallest addr in mem (out of the 8), 
@@ -298,31 +298,14 @@ let step (m:mach) : unit =
       | _ -> failwith "not an InsB0"
     end in
   let (opcode, ls) = get_ins Rip regs mem in
-  let get_mem_idx (op:operand) : int = 
-    let idx = 
-      begin match op with
-        | Ind1 i      -> imm_to_quad i
-        | Ind2 r      -> regs.(rind r)
-        | Ind3 (i, r) -> (Int64.add (imm_to_quad i) regs.(rind r))
-        | _ -> failwith "can't idx reg or imm"
-      end
-    in Int64.to_int idx
-  in 
   let get_mem_from_idx (idx:int) : int64 = 
       int64_of_sbytes (Array.to_list (Array.sub mem idx 8)) in
   let get_mem (op:operand) : int64 = 
     begin match op with
       | Imm i       -> imm_to_quad i
       | Reg r       -> regs.(rind r)
-      | _ -> get_mem_from_idx (get_mem_idx op)
+      | _ -> get_mem_from_idx (get_addr op regs)
     end
-  in
-  let set_mem (op:operand) (v:quad) : unit = 
-    begin match op with
-      | Imm i -> failwith "can't set an Immediate"
-      | Reg r -> Array.set regs (rind r) v
-      | _ -> quad_sbyte_list_into_mem (sbytes_of_int64 v) mem (get_mem_idx op)
-    end 
   in
   let set_sz_flags (res:int64) : unit = 
     if(res = 0L) then (flags.fz <- true;) else (flags.fz <- false);
@@ -331,7 +314,7 @@ let step (m:mach) : unit =
     let shift = Int64.to_int (get_mem src) in
     let original = get_mem dst in
     let res = func original shift in
-    set_mem dst res;
+    set_mem (quad_to_imm res) dst regs mem;
     (*L: idea: AND it with 0b110...0 and then XOR it with that number, still wrong*)
     let one_one_zeros : int64 = Int64.shift_right Int64.min_int 1 in
     let first_two_bits_diff : bool = 
@@ -353,18 +336,18 @@ let step (m:mach) : unit =
     (*test if func is imul instr => fz fs ignored in that case*)
     if(((func 1L 1L).value) <> 1L) then set_sz_flags res.value;
     flags.fo <- res.overflow;
-    if(written_to_mem) then set_mem op2 (res.value)
+    if(written_to_mem) then set_mem op2 (quad_to_imm res.value) regs mem
   in
   let un_arithm_ovf (op:operand) (func: int64 -> Ovf.t) : unit = 
     let res = func (get_mem op) in
     set_sz_flags res.value;
     flags.fo <- res.overflow;
-    set_mem op (res.value) in
+    set_mem op (quad_to_imm res.value) regs mem in
    let bin_log (src:operand) (dst:operand) (func: int64 -> int64 -> int64) : unit =
     flags.fo <- false;
-    set_mem dst (func (get_mem src) (get_mem dst)) in
+    set_mem dst (quad_to_imm (func (get_mem src) (get_mem dst))) regs mem in
   begin match (opcode, ls) with
-    | (Movq, [op1; op2])  -> movq_helper op1 op2 regs mem
+    | (Movq, [op1; op2])  -> set_mem op1 op2 regs mem
     | (Pushq, [op])       -> pushq_helper op regs mem
     | (Popq, [op])        -> popq_helper op regs mem
     | (Leaq, [ind; op2])  -> leaq_helper ind op2 regs mem
@@ -372,7 +355,7 @@ let step (m:mach) : unit =
     | (Decq, [op])        -> un_arithm_ovf op Ovf.pred
     | (Negq, [op])        -> un_arithm_ovf op Ovf.neg
     | (Notq, [op])        -> flags.fo <- false;
-                             set_mem op (Int64.lognot (get_mem op)) 
+                             set_mem op (quad_to_imm (Int64.lognot (get_mem op))) regs mem 
     | (Addq, [op1; op2])  -> bin_arithm_ofv op1 op2 Ovf.add true
     | (Subq, [op1; op2])  -> bin_arithm_ofv op1 op2 Ovf.sub true
     | (Imulq, [op1; op2]) -> bin_arithm_ofv op1 op2 Ovf.mul true
