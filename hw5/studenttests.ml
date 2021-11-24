143cb98d65e7c6f2b4ef82db4f1fa31a408c4578
leon <plathl@student.ethz.ch>
2021-11-24 11:26:11 +0100

    added first basic student tests

--- hw5/studenttests.ml
+++ hw5/studenttests.ml
@@ -1,10 +1,33 @@
 open Assert
+open X86
+open Driver
+open Ll
+open Backend
 
 (* These tests are provided by you -- they will be graded manually *)
 
 (* You should also add additional test cases here to help you   *)
 (* debug your program.                                          *)
 
+let unit_tests = [
+    "subtype_string_stringQ",
+    (fun () ->
+        if Typechecker.subtype Tctxt.empty (TRef RString) (TNullRef RString) then ()
+        else failwith "should not fail")                                                                                     
+  ; ("no_subtype_bool_int",
+    (fun () ->
+        if Typechecker.subtype Tctxt.empty (TBool) (TInt) then
+          failwith "should not succeed" else ())
+    )
+  ; ("no_subtype_string_bool",
+  (fun () ->
+      if Typechecker.subtype Tctxt.empty (TRef RString) (TBool) then
+        failwith "should not succeed" else ())
+  )
+  ]
+
 let provided_tests : suite = [
-  
+  GradedTest("STUDENT subtype unit tests", 3, unit_tests);
 ] 
+
+
